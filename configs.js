c54dfbbe197d11df8b1f1b213b85660f90a02a90
Phạm Văn Trường <invncur@gmail.com>
2017-09-03 17:52:51 +0700

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,1 +1,2 @@
-var configs = {server:"http://domain_hay_ip_của_máy_chủ:1985/",database:"database_cần_xem_báo_cáo"}
+var configs = {server:"http://domain_hay_ip_của_máy_chủ:1985/",database:"database_cần_xem_báo_cáo"};
+//chú ý: Phải mở cổng 1985 trên máy chủ
